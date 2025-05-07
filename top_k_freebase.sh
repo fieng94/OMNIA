db03037c5ae11cc50428cddb4dff2c93acd719e8
fieng <fieng@MacBook-Pro-de-cdc.local>
2025-05-07 14:39:13 +0200

    fix top-k experiment

--- top_k_freebase.sh
+++ top_k_freebase.sh
@@ -5,5 +5,5 @@ data='freebase'
 # Loop through the array and run the Python program with each top-k value
 for k in "${top_k[@]}"; do
     echo "Testing at top $k"
-    python3 omnia_top_k.py --path "data/${data}/data_sample.csv" --output_dir "results/top_k/${data}" --setting triples --top-k $k
+    python3 omnia_top_k.py --path "data/${data}/data_sample.csv" --output_dir "results/top_k/${data}" --setting triples --top_k $k
 done
