f58ddebc47bb5cba940f407f282d8edc766b6d07
fieng <fieng@MacBook-Pro-de-cdc.local>
2025-10-17 08:55:02 +0200

    add timer for cand gen

--- cand_gen.py
+++ cand_gen.py
@@ -8,9 +8,10 @@ import experiment.result as result
 import numpy as np
 import pandas as pd
 import os
+import time
 
 def gen_cand(dataset:str):
-    path =  f'data/{dataset}/data.csv'
+    path =  f'data/{dataset}/data_sample.csv'
     # Read data
     print(f'Reading data at {path}')
     df = pd.read_csv(path)
@@ -19,16 +20,34 @@ def gen_cand(dataset:str):
     evaluation_df, candidates_df, missing_df = preprocess.create_experiment_df(path)   
     return df, evaluation_df, candidates_df, missing_df
 
-def filtrering_df(df, evaluation_df, missing_df):
+def filtrering_df(df, evaluation_df, missing_df, sample_size):
     print('Filtering using Knowledge Graph Embedding (TransE)')
     filtred_df = filtering.create_filtred_df(df, evaluation_df, missing_df)
     filtred_df = filtred_df.merge(evaluation_df, how='left')
-    filtred_df_sample = filtering.create_sample(filtred_df,sample_size= 500, true_cand_ratio= 0.5)
+    filtred_df_sample = filtering.create_sample(filtred_df,sample_size= sample_size, true_cand_ratio= 0.5)
     return filtred_df, filtred_df_sample
 
+records = []
+
 dataset_list = ['freebase','wordnet','codex-m']
+sample_size_list = [500,1000,1500]
 for dataset in dataset_list:
-    df, evaluation_df, candidates_df, missing_df = gen_cand(dataset)
-    filtred_df, filtred_df_sample = filtrering_df(df, evaluation_df, missing_df)
-    cand_path = f'data/{dataset}/cand_sample.csv'
-    filtred_df_sample.to_csv(cand_path, index=None)
+        for sample_size in sample_size_list:
+            start = time.perf_counter()
+            df, evaluation_df, candidates_df, missing_df = gen_cand(dataset)
+            filtred_df, filtred_df_sample = filtrering_df(df, evaluation_df, missing_df, sample_size)
+            cand_path = f'data/{dataset}/cand_sample_{sample_size}.csv'
+            filtred_df_sample.to_csv(cand_path, index=None)
+            end = time.perf_counter()
+            print(f"Execution time: {end - start:.6f} seconds\tDataset:{dataset}\tSample size:{sample_size}")
+            records.append({
+            "dataset": dataset,
+            "sample_size": sample_size,
+            "execution_time_sec": end - start
+            })
+
+# Convert to DataFrame
+time_df = pd.DataFrame(records)
+
+# Save as CSV for later analysis
+time_df.to_csv("timing_results.csv", index=False)
